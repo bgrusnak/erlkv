556af2115253a4355ddd6fa2a3b6bbd3a8dd2aa1
Ilya A. Shlyakhovoy <ilyacat@gmail.com>
2016-07-27 23:18:47 +0300

    Completed

--- src/resource_handler.erl
+++ src/resource_handler.erl
@@ -4,20 +4,38 @@
 -export([init/3]).
 -export([allowed_methods/2]).
 -export([content_types_provided/2]).
-
-
+-export([content_types_accepted/2]).
+-export([resource_exists/2]).
 
 
 init(_Transport, Req, []) ->
 	case cowboy_req:method(Req) of
 		{<<"DELETE">>, _} ->
-			handle_delete(Req1);
+			handle_delete(Req);
 		{<<"GET">>, Req1} ->
-			handle_get(Req1)
-	end.
-     
+			handle_get(Req)
+end.
+
 allowed_methods(Req, State) ->
-	{[ <<"GET">>, <<"DELETE">>], Req, State}.
+	{[ <<"GET">>,  <<"DELETE">>], Req, State}.
+
+content_types_provided(Req, State) ->
+	{[
+		{{<<"text">>, <<"html">>, []}, pass_rest},
+		{{<<"text">>, <<"plain">>, []}, pass_rest}
+	], Req, State}.
+	
+content_types_accepted(Req, State) ->
+	{[{{<<"application">>, <<"x-www-form-urlencoded">>, []}, create_res}],
+		Req, State}.
+
+resource_exists(Req, _State) ->
+	case cowboy_req:binding(res_id, Req) of
+		{undefined, Req2} ->
+			{false, Req2, index};
+		{ReqId, Req2} ->
+			{db:is_item_exists(ReqId), Req2, ReqId}
+	end.
 
 handle_delete(Req) ->
 	case cowboy_req:binding(res_id, Req) of
